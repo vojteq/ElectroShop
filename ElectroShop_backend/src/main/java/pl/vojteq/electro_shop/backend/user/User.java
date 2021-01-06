3810f998847feea98466de29a37f26012a33a19c
Wojciech Ankus <305267@student.agh.edu.pl>
2021-01-06 11:52:37 +0100

    empty user class added

---
+++ ElectroShop_backend/src/main/java/pl/vojteq/electro_shop/backend/user/User.java
@@ -0,0 +1,15 @@
+package pl.vojteq.electro_shop.backend.user;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+}
