608dc44547f2866a17073b2c8784f9dbcdce7a55
Wojciech Ankus <305267@student.agh.edu.pl>
2021-01-06 11:49:16 +0100

    backend added

---
+++ ElectroShop_backend/src/main/java/pl/vojteq/electro_shop/backend/ElectroShopBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.vojteq.electro_shop.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectroShopBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElectroShopBackendApplication.class, args);
+    }
+
+}
---
+++ ElectroShop_backend/src/test/java/pl/vojteq/electro_shop/backend/ElectroShopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.vojteq.electro_shop.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectroShopBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
